2de8fe2a29df0cd110c7f86fe10bb8c470ee1619
sai236 <569284969@qq.com>
2019-07-14 21:35:40 +0800

    dd

---
+++ src/main/java/com/jenkins/TestNg3.java
@@ -0,0 +1,15 @@
+package com.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNg3 {
+	
+	@Test
+	public void t() {
+		System.err.println("*****************************");
+		System.out.println("TestNg2TestNg2TestNg2TestNg2TestNg2TestNg2");
+		Assert.assertTrue(true);
+		System.err.println("*****************************");
+	}
+}
---
+++ src/main/java/com/jenkins/TestNg4.java
@@ -0,0 +1,15 @@
+package com.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNg4 {
+	
+	@Test
+	public void t() {
+		System.err.println("*****************************");
+		System.out.println("TestNg2TestNg2TestNg2TestNg2TestNg2TestNg2");
+		Assert.assertTrue(false);
+		System.err.println("*****************************");
+	}
+}
