7ed62285aa7e4c2fdc96773a4d4027b516350f5a
sai236 <569284969@qq.com>
2019-07-14 21:01:24 +0800

    RR

--- src/main/java/com/jenkins/TestNg2.java
+++ src/main/java/com/jenkins/TestNg2.java
@@ -3,12 +3,12 @@ package com.jenkins;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestNg1 {
+public class TestNg2 {
 	
 	@Test
 	public void t() {
 		System.err.println("*****************************");
-		System.out.println("TestNg1TestNg1TestNg1TestNg1TestNg1TestNg1");
+		System.out.println("TestNg2TestNg2TestNg2TestNg2TestNg2TestNg2");
 		Assert.assertTrue(true);
 		System.err.println("*****************************");
 	}
