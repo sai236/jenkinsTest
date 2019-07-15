57396a186ea01f79ccffbc49cb99a52260f13018
sai236 <569284969@qq.com>
2019-07-15 21:28:07 +0800

    ee

---
+++ src/main/java/com/jenkins/TestNg7.java
@@ -0,0 +1,15 @@
+package com.jenkins;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNg7 {
+	
+	@Test
+	public void t() {
+		System.err.println("*****************************");
+		System.out.println("TestNg2TestNg2TestNg2TestNg2TestNg2TestNg2");
+		Assert.assertTrue(true);
+		System.err.println("*****************************");
+	}
+}
